ca8ecfd2abc293eb31664ff8d745e3f7fef2ad7f
Ed <oflynned@tcd.ie>
2017-05-15 20:25:14 +0100

    added base project of storyboard of skeleton layout for iOS. I know I'll be cringing at this by the end of this :D

---
+++ iOS/Diabetes/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Diabetes
+//
+//  Created by Ed on 09/05/2017.
+//  Copyright © 2017 GlassByte. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
