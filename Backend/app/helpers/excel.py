de47621ce61ac46e1b6fd27364221c228c8e49fb
Ed <oflynned@tcd.ie>
2017-07-02 20:26:56 +0100

    parsing the info sheet is done, yet to test and throw it into the json generator function for saving it to file

--- Backend/app/helpers/excel.py
+++ Backend/app/helpers/excel.py
@@ -5,14 +5,12 @@ import os
 
 class Excel:
     @staticmethod
-    def __get_working_dir(file_name):
-        return "app/datasets/" + file_name
+    def __get_dataset_working_dir(file_name):
+        return os.getcwd() + "/app/datasets/" + file_name
 
     @staticmethod
     def __retrieve_data_sheets(workbook):
-
         jsonified_output_data = []
-
         sheet = workbook.sheet_by_index(7)
 
         # let's get the heading names first so we know
@@ -56,7 +54,7 @@ class Excel:
             min_row = exercise[1]
             max_row = exercise[2]
 
-            # iterate two-dimensionally over the x and y axes
+            # iterate two-dimensionally over the x and y axes, while iterating over the range of exercises
             # type, intensity, duration and suggestion are common
             # 4 cols indicates the sheet is normal, 5 cols indicate an extra parameter in col 4
             # where the suggestion is then given in col 5
@@ -68,10 +66,11 @@ class Excel:
             heading_exclusions = ['', 'Suggestions']
 
             is_parametrised = (len(headings) > 4)
+            max_col = 5 if is_parametrised else 4
 
             # extra parameter in col 4
             for data_row in range(min_row, max_row):
-                data = sheet.row_values(data_row, 0, 5)
+                data = sheet.row_values(data_row, 0, max_col)
                 for index, row_value in enumerate(data):
                     # before meal, after meal, don't care
                     # * indicates showing a warning with the suggestion
@@ -83,6 +82,11 @@ class Excel:
                         row_value = row_value.strip()
                         groomed.append(row_value)
 
+                # TODO sanitise the incorrect extra underscores
+                # TODO check BG params are passed okay
+                # TODO issue on some suggestions not appearing
+
+                # given the 5th row, sanitise the content for the suggestion object
                 if is_parametrised:
                     parameter_content = data[3].strip()
                     suggestion_content = data[4].strip()
@@ -105,20 +109,56 @@ class Excel:
 
                         suggestions.append(suggestion_object)
 
-            if is_parametrised:
-                data_object["exercise_meal_suggestions"] = suggestions
-
             data_object["exercise_type"] = Excel.__groom_titles(groomed[0])
             data_object["exercise_intensity"] = Excel.__split_tags(Excel.__groom_titles(groomed[1]))
             data_object["exercise_duration"] = Excel.__groom_duration_timings(groomed[2])
+
+            if is_parametrised:
+                data_object["exercise_meal_suggestions"] = suggestions
 
             jsonified_output_data.append(data_object)
 
         return jsonified_output_data
 
     @staticmethod
+    def __is_link(row):
+        if row is not '':
+            return row[:4] == "http"
+
+        return False
+
+    @staticmethod
     def __groom_info_page(raw_data):
-        pass
+        # data only exists in the 0th col over n rows
+        output_data = []
+        initial_groomed_content = []
+
+        for r in range(raw_data.nrows):
+            raw_row_content = raw_data.row_values(r, 0)
+            if raw_row_content is not '':
+                groomed_row_content = raw_row_content[0].strip()
+                initial_groomed_content.append(groomed_row_content)
+
+        # groom over the data to split it by white spaces
+        # this allows us to assume that 0th element is the title
+        links = []
+        section_name = ""
+
+        for i, content in enumerate(initial_groomed_content):
+            # content is valid
+            if not Excel.__is_link(content):
+                # create a new section for the new list of links
+                if content is not '':
+                    links = []
+                    section_name = content
+            else:
+                links.append(content)
+
+            # time to flush the content to the list
+            if content is '' or content is None or (i == len(initial_groomed_content) - 1):
+                output_data.append({"section_name": section_name, "links": links})
+
+        return output_data
 
     @staticmethod
     def __groom_duration_timings(timing):
@@ -168,7 +208,7 @@ class Excel:
     @staticmethod
     def groom_content():
         dataset_file_name = "app_sug_2.xlsx"
-        dataset_location = Excel.__get_working_dir(dataset_file_name)
+        dataset_location = Excel.__get_dataset_working_dir(dataset_file_name)
 
         workbook = xlrd.open_workbook(dataset_location)
 
@@ -177,3 +217,7 @@ class Excel:
         for i in range(workbook.nsheets - 1):
             groomed_jsonified_data = Excel.__retrieve_data_sheets(workbook)
             Excel.__save_json_to_file(workbook.sheet_by_index(i).name, groomed_jsonified_data)
+
+        # handle the info page now and generate its own representation
+        info_page_data = workbook.sheet_by_index(8)
+        return Excel.__groom_info_page(info_page_data)
