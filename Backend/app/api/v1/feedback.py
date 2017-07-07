754e84bdd25bfa36be34198d85cfb140f88afde0
Ed <oflynned@tcd.ie>
2017-07-07 17:56:25 +0100

    added shell method for hypo/target helper function and finished endpoints for aggregation for plans and feedback

--- Backend/app/api/v1/feedback.py
+++ Backend/app/api/v1/feedback.py
@@ -6,14 +6,15 @@ from app.app import mongo
 feedback_endpoint = Blueprint("feedback", __name__)
 
 
-# POST { "plan_id": <oid>, "rating": [0 .. 4], "comment": <string> }
+# POST { "plan_id": <oid>, "rating": [0 .. 4], "comment": <string>, "email": <string> }
 @feedback_endpoint.route("/create", methods=["POST"])
 def create_feedback():
     data = request.json
     plan_id = data["plan_id"]
     rating = int(data["rating"])
+    email = str(data["email"])
 
-    feedback = {"plan_id": plan_id, "rating": rating}
+    feedback = {"plan_id": plan_id, "rating": rating, "email": email}
 
     if "comment" in data:
         feedback["comment"] = str(data["comment"])
@@ -22,20 +23,29 @@ def create_feedback():
     return Content.get_json({"success": True})
 
 
+# POST { "email": <string> }
 @feedback_endpoint.route("/get", methods=["POST"])
 def get_past_feedback():
     data = request.json
     email = data["email"]
 
     aggregated_history_feedback = []
-
-    # first find the user's plans
     exercise_plans = list(mongo.db.plans.find({"email": email}))
+    past_feedback = list(mongo.db.feedback.find({"email": email}))
 
-    return Content.get_json(exercise_plans)
+    for plan in exercise_plans:
+        for feedback in past_feedback:
+            if str(plan["_id"]) == str(feedback["plan_id"]):
+                aggregated_plan = plan
+                aggregated_plan["feedback"] = feedback
+                aggregated_plan["feedback"].pop("_id")
+                aggregated_plan["feedback"].pop("plan_id")
+                aggregated_plan["feedback"].pop("email")
+                aggregated_history_feedback.append(aggregated_plan)
+
+    return Content.get_json(aggregated_history_feedback)
 
 
-# POST {"email": "..."}
-# RETURN {"result": <boolean>}
-def has_user_been_below_or_above_target_or_hypo_last_24_hrs():
-    pass
+@feedback_endpoint.route("/get-all", methods=["GET"])
+def get_all_feedback():
+    return Content.get_json(list(mongo.db.feedback.find()))
--- Backend/app/api/v1/plan.py
+++ Backend/app/api/v1/plan.py
@@ -16,15 +16,24 @@ class Plan:
         plan_object["plan"] = data
         mongo.db.plans.save(plan_object)
 
+    @staticmethod
+    def has_user_been_below_or_above_target_or_hypo_last_24_hrs(email):
+        twenty_four_hours_in_millis = 1000 * 60 * 60 * 24
+        time_threshold = Content.current_time_in_millis() - twenty_four_hours_in_millis
+
+        # TODO define hypo and outside target, as I need to query past BG values
+        bg_query = {"$gt": 15, "$lt": 7}
+        query = {"email": email, "time": {"$gt": time_threshold}, "plan": {"bg_level": bg_query}}
+        hazardous_bg_plans = list(mongo.db.plans.find(query))
+        return len(hazardous_bg_plans) > 0
+
 
 # POST { "email": "..." }
 @plan_endpoint.route("/get", methods=["POST"])
 def get_plans():
     data = request.json
     email = data["email"]
-
     plans = list(mongo.db.plans.find({"email": email}))
-    print(plans)
     return Content.get_json(plans)
 
 
